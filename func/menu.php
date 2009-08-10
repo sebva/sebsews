f0ff592bfd1e9f2424247e6233a269c92295762a
sebyx <sebyx@97b18dd2-2e1a-11df-a8e3-ab3d4dd5af44>
2009-08-10 10:52:21 +0000

    Inclusion d'un moteur de template (LiteTemplate v1.9)+modifications mineures

--- func/menu.php
+++ func/menu.php
@@ -2,22 +2,13 @@
 if($page!='menu')
 {
 	$reponse = mysql_query('SELECT title,shorttitle FROM '.$mysqlTablePages.' ORDER BY id');
+	$menuNomCourt=array();
+	$menuNomLong=array();
 	while ($donnees = mysql_fetch_array($reponse) )
 	{
-		switch($donnees['shorttitle'])		
-		{	//S'il y a des sous-menus à créer
-			case 'news':
-				$menu.='<li id="news">
-						<a href="http://'.$domaine.$repertoire;$menu.=($rewrite=='on') ?'/news.html' :'/?page=news';$menu.='">News</a>
-						<ul class="sousmenu">
-							<li><a href="http://'.$domaine.$repertoire;$menu.=($rewrite=='on') ?'/news.xml':'/rss.php';$menu.='">Flux RSS</a></li>
-						</ul>
-					</li>';
-				break;		
-			default:
-				$menu.='<li><a href="http://'.$domaine.$repertoire.'/';$menu.=($rewrite=='on') ?'':'?page=';$menu.=$donnees['shorttitle'];$menu.=($rewrite=='on') ?'.html':'';$menu.='">'.$donnees['title'].'</a></li>';
-				break;
-		}
+		$menuNomCourt[]=$donnees['shorttitle'];
+		$menuNomLong[]=$donnees['title'];
+		$iCompt++;
 	}
 }
 else
---
+++ templates/default/footer.php
@@ -0,0 +1,9 @@
+<?php require('config.php'); ?>
+
+<!--Copyright-->&copy; Copyright <?php echo date(Y)?> - <a href="http://<?php echo $domaine.$repertoire?>"><?php echo $nom?></a>&nbsp;-&nbsp;
+					<!--Veuillez SVP laisser cette indication, Merci--><a href="http://sebseasywebsite.sourceforge.net/">Powered by Séb's EasyWebSite</a><span class="finfooter">&nbsp;-&nbsp;
+					<!--Administration--><a href="http://<?php echo $domaine.$repertoire?>/admin/">Administration</a>&nbsp;-&nbsp;
+					<!--RSS--><a href="http://<?php echo $domaine.$repertoire;echo ($rewrite=='on') ?'/news.xml':'/rss.php';?>"><img src="http://<?php echo $domaine.$repertoire.'/templates/'.$template?>/images/feedicon.png" alt="Flux RSS" height="20" width="20" /></a>
+					<!--Valid RSS--><a href="http://validator.w3.org/feed/check.cgi?url=http%3A//<?php echo $domaine.$repertoire;echo ($rewrite=='on') ?'/news.xml':'/rss.php';?>"><img src="http://<?php echo $domaine.$repertoire.'/templates/'.$template?>/images/valid-rss.png" alt="Valid RSS" height="20" width="59" /></a>
+					<!--Valid XHTML--><a href="http://validator.w3.org/check?uri=referer"><img src="http://<?php echo $domaine.$repertoire.'/templates/'.$template?>/images/valid-xhtml11.png" alt="XHTML 1.1 Valide" height="20" width="59" /></a>
+					<!--Valid CSS--><a href="http://jigsaw.w3.org/css-validator/check/referer"><img width="59" height="20" src="http://<?php echo $domaine.$repertoire.'/templates/'.$template?>/images/valid-css2.png" alt="CSS 2.1 Valide !" /></a></span>
