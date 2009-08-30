02037ef42d8be215dc6c5ccd496231e3bc26815b
sebyx <sebyx@97b18dd2-2e1a-11df-a8e3-ab3d4dd5af44>
2009-08-30 22:21:26 +0000

    Remplacement de FCKEditor par CKEditor

--- admin/news_create.php
+++ admin/news_create.php
@@ -31,14 +31,11 @@ CURRENT_TIMESTAMP , '".$_POST['link']."', '".mysql_real_escape_string($_POST['au
 	La news doit-elle être affichée directement lors du listage des news ou affichée limitée à 100 caractères et avoir un lien vers la news complète ?<br/>
 	<label>Affichée directement</label><input type="radio" name="link" value="0"/>
 	<label>Limitée à 100 caractères avec lien</label><input type="radio" name="link" value="1" checked="checked" /></p>
-	<?php
-	require_once("fckeditor/fckeditor.php") ;
-	$oFCKeditor = new FCKeditor('text') ;
-	$oFCKeditor->BasePath = 'fckeditor/' ;
-	$oFCKeditor->Height = 420;
-	$oFCKeditor->Value = '';
-	$oFCKeditor->Create() ;
-	?><input type="submit" value="Envoyer"/></form>
+	<textarea name="text"></textarea>
+	<script type="text/javascript">
+		CKEDITOR.replace( 'text' );
+	</script>
+	<input type="submit" value="Envoyer"/></form>
 <?php }
 echo '<br/><a href="news_index.php">Retour page précédente</a>';
 require 'footer.php'; ?>
