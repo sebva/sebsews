ae45ba08154bf2cfecdc22071bf20ff9589c34ff
Sébastien Vaucher <sebyx31@users.noreply.github.com>
2009-09-02 20:30:27 +0000

    Modification de la structure des fichiers de l'administration (avec ajout de la fonction wipe)
    git-svn-id: http://sebsews.googlecode.com/svn@26 97b18dd2-2e1a-11df-a8e3-ab3d4dd5af44

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,8 @@
 <?php  require 'header.php';?>
 	<ul>
-		<li><a href="page_index.php">Gestion des pages</a></li>
-		<li><a href="news_index.php">Gestion des news</a></li>
-		<li><a href="template_change.php">Changement du template</a></li>
+		<li><a href="page.php">Gestion des pages</a></li>
+		<li><a href="news.php">Gestion des news</a></li>
+		<li><a href="settings.php">Préférences générales</a></li>
 		<?php if ($googleAppsDomain!=''){?><li><a href="https://www.google.com/a/<?php echo $googleAppsDomain?>/ServiceLogin">Google Apps</a></li><?php }?>
 	</ul>
 <?php require 'footer.php'; ?>
