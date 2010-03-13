e89d08c7201b9c73f764ef2c4df1ab412f09c228
seb.vaucher <seb.vaucher@97b18dd2-2e1a-11df-a8e3-ab3d4dd5af44>
2010-03-13 11:34:10 +0000

    Le flux RSS est maintenant mis en cache. Ajout de mkdir dans cache.php

--- trunk/rss.php
+++ trunk/rss.php
@@ -1,42 +1,51 @@
-<?php 
-require_once('config.php');
-header('Content-Type: application/rss+xml; charset=utf-8');
-echo'<?xml version="1.0" encoding="utf-8"?>'."\n"
-?>
-<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-        <title><![CDATA[<?php echo $nom?> :: News]]></title>
-        <description><![CDATA[News de <?php echo $nom?>]]></description>
-		<language>fr-ch</language>
-        <lastBuildDate><?php echo date(r);?></lastBuildDate>
-        <link>http://<?php echo $domaine.$repertoire;echo ($rewrite=='on') ?'/news.html':'/?page=news';?></link>
-		<atom:link href="http://<?php echo $domaine.$_SERVER['REQUEST_URI']?>" rel="self" type="application/rss+xml" />
-		<image>
-			<title><![CDATA[<?php echo $nom?> :: News]]></title>
-			<url>http://<?php echo $domaine.$repertoire?>/rsslogo.png</url>
-			<link>http://<?php echo $domaine.$repertoire;echo ($rewrite=='on') ?'/news.html':'/?page=news';?></link>
-			<width>144</width>
-			<height>53</height>
-		</image>
+<?php
+require('func/cache.php');
+echo Cache('getRSS()', 60);
 
-		<?php mysql_connect($mysqlHost, $mysqlUser, $mysqlPassword);
-		mysql_set_charset ('UTF8');
-		mysql_select_db($mysqlDb);
-		$reponse = mysql_query('SELECT * FROM '.$mysqlTableNews.' ORDER BY date desc');
-		while ($donnees = mysql_fetch_array($reponse) )
-		{
-			$timestamp = strtotime($donnees['date']);
-			$text= "\n".'<item>
-					<title><![CDATA['.$donnees['title'].']]></title>
-					<link>http://'.$domaine.$repertoire;$text.= ($rewrite=='on') ?'/news/':'/?page=news&amp;news=';$text.= $donnees['id'].'</link>
-					<guid>http://'.$domaine.$repertoire;$text.= ($rewrite=='on') ?'/news/':'/?page=news&amp;news=';$text.= $donnees['id'].'</guid>
-					<description><![CDATA['.str_replace('$repertoire', $repertoire, str_replace('$domaine', $domaine, $donnees['text'])).']]></description>
-					<pubDate>'.date(r, $timestamp).'</pubDate>
-					<author>'.strtolower(str_replace(' ', '.' ,$donnees['auteur'])).'@'.$emailDomaine.' ('.$donnees['auteur'].')</author>
-					<category>'.$donnees['categorie'].'</category>
-				</item>';
-			echo stripslashes($text);
-		}
-		?>
-    </channel>
-</rss>
+function getRSS()
+{
+	require_once('config.php');
+	header('Content-Type: application/rss+xml; charset=utf-8');
+	$return.='<?xml version="1.0" encoding="utf-8"?>'."\n";
+	$return.= '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+		<channel>
+			<title><![CDATA['.$nom.' :: News]]></title>
+			<description><![CDATA[News de '.$nom.']]></description>
+			<language>fr-ch</language>
+			<lastBuildDate>'.date(r).'</lastBuildDate>
+			<link>http://'.$domaine.$repertoire;
+			$return.= ($rewrite=='on') ?'/news.html':'/?page=news';
+			$return.='</link>
+			<atom:link href="http://'.$domaine.$_SERVER['REQUEST_URI'].'" rel="self" type="application/rss+xml" />
+			<image>
+				<title><![CDATA['.$nom.' :: News]]></title>
+				<url>http://'.$domaine.$repertoire.'/rsslogo.png</url>
+				<link>http://'.$domaine.$repertoire;
+				$return.= ($rewrite=='on') ?'/news.html':'/?page=news';
+				$return.='</link>
+				<width>144</width>
+				<height>53</height>
+			</image>';
+
+			mysql_connect($mysqlHost, $mysqlUser, $mysqlPassword);
+			mysql_set_charset ('UTF8');
+			mysql_select_db($mysqlDb);
+			$reponse = mysql_query('SELECT * FROM '.$mysqlTableNews.' ORDER BY date desc');
+			while ($donnees = mysql_fetch_array($reponse) )
+			{
+				$timestamp = strtotime($donnees['date']);
+				$text= "\n".'<item>
+						<title><![CDATA['.$donnees['title'].']]></title>
+						<link>http://'.$domaine.$repertoire;$text.= ($rewrite=='on') ?'/news/':'/?page=news&amp;news=';$text.= $donnees['id'].'</link>
+						<guid>http://'.$domaine.$repertoire;$text.= ($rewrite=='on') ?'/news/':'/?page=news&amp;news=';$text.= $donnees['id'].'</guid>
+						<description><![CDATA['.str_replace('$repertoire', $repertoire, str_replace('$domaine', $domaine, $donnees['text'])).']]></description>
+						<pubDate>'.date(r, $timestamp).'</pubDate>
+						<author>'.strtolower(str_replace(' ', '.' ,$donnees['auteur'])).'@'.$emailDomaine.' ('.$donnees['auteur'].')</author>
+						<category>'.$donnees['categorie'].'</category>
+					</item>';
+				$return.=stripslashes($text);
+			}
+		$return.='</channel>
+	</rss>';
+	return $return;
+}
