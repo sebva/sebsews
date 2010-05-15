0bbc5bc88e177ba45d1ae6a5ac3566fcd0ba9346
Sébastien Vaucher <sebyx31@users.noreply.github.com>
2010-05-15 16:26:13 +0000

    Added GPLv3 comment at the top of each file.
    git-svn-id: http://sebsews.googlecode.com/svn/trunk@65 97b18dd2-2e1a-11df-a8e3-ab3d4dd5af44

--- admin/settings.php
+++ admin/settings.php
@@ -1,4 +1,22 @@
-<?php //settings.php
+<?php
+/*
+	Séb's Easy Website, a tiny CMS for your personal website
+	Copyright (C) 2010 Sébastien Vaucher
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+//settings.php
 require 'header.php';
 
 switch($_GET['action'])
--- templates/head.php
+++ templates/head.php
@@ -1,4 +1,22 @@
-<?php require('config.php'); ?><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+/*
+	Séb's Easy Website, a tiny CMS for your personal website
+	Copyright (C) 2010 Sébastien Vaucher
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+require('config.php'); ?><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
 if ($googleWebmasterToolsMetaTag !=''){?><meta name="verify-v1" content="<?php echo $googleWebmasterToolsMetaTag.'" />';}?>
 		<link rel="icon" href="http://<?php echo $domaine.$repertoire?>/favicon.png" />
